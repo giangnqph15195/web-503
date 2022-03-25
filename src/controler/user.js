1f535fd3f9536ac88ef09434abfa8230d43d8737
giangnqph15195 <giangqu13579@gmail.com>
2022-03-25 07:29:19 +0700

    SingUp

--- src/controler/user.js
+++ src/controler/user.js
@@ -1,18 +1,34 @@
 import User from "../models/user";
 
-export const List = async (req,res) =>{
+export const Signin = async (req,res) =>{
     try {
-        const user = await User.find().exec()
+        const {name, email , hash_pass} = req.body
+        const user = await User.findOne().exec()
         res.json(user)
     } catch (error) {
         
     }
 }
 
-export const Add = async (req, res) =>{
+export const Signup = async (req, res) =>{
+    const {name, email , password} = req.body;
     try {
-        const user = await new User(req.body).save()
-        res.json(user)
+        const Check = await User.findOne({email}).exec()
+        if(Check){
+            res.json({
+                message: "Email da ton tai"
+            })
+        }else{
+            const user = await new User({name, email, password}).save()
+            res.json({
+                user: {
+                    _id: user._id,
+                    name:user.name,
+                    email:user.email,
+                    password:user.hash_pass
+                }
+            })
+        }
     } catch (error) {
         
     }
--- src/router/user.js
+++ src/router/user.js
@@ -1,11 +1,11 @@
 const {Router} = require ('express')
-const { List, Add } = require('../controler/user')
+const { Signup, Signin } = require('../controler/user')
 
 
 const router = Router()
 
-router.get("/api/users", List)
-router.post("/api/user", Add)
+router.get("/api/users", Signin)
+router.post("/api/user", Signup)
 
 
 module.exports = router
