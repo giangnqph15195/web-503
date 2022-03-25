504d30afd84e9e4be9a29cfe1a76b9e456d76d17
giangnqph15195 <giangqu13579@gmail.com>
2022-03-25 07:28:57 +0700

    update singup

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import {createHmac} from 'crypto';
 
 const UserSchema =  new Schema({
     name:{
@@ -11,11 +12,31 @@ const UserSchema =  new Schema({
         required:true,
         minLength: 10
     },
-    hash_pass:{
+    password:{
         type: String,
         required: true,
         minLength:8
     }
 }, {timestamps:true})
+
+UserSchema.methods={
+    encryptPass(password){
+        if(!password) return;
+        try {
+            return createHmac('sha256', "abc").update(password).digest('hex');
+        } catch (error) {
+            
+        }
+    }
+} 
+UserSchema.pre("save", async function save(next){
+    try {
+        this.password = this.encryptPass(this.password)
+        return next(); 
+    } catch (error) {
+        
+    }
+})
+
 
 export default mongoose.model("User", UserSchema)
