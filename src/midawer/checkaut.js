152647af0afcd09f03535fe3739e81b4287131eb
giangnqph15195 <giangqu13579@gmail.com>
2022-03-25 19:42:00 +0700

    welcom

--- src/midawer/checkaut.js
+++
@@ -1,9 +0,0 @@
-import expressJWT from 'express-jwt'
-export const requiredSingin = expressJWT({
-    secret: "123456",
-    algorithms:["HS256"],
-    requestProperty: "abc                                   "
-})
-export const Auth = (req,res,next) =>{
-    console.log(req.abc)
-}
